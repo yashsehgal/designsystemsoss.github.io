0ed46b39401b09d1a18a947038ec93b1c6917f57
yashsehgal <yashsehgal.work@gmail.com>
2021-04-15 15:40:54 +0530

    paths are modified in the Header Component

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ import Logo from './logo-github.jpg';
 const Header =() => {
   return (
     <div className="Header">
-      <a href="/">
+      <a href="/community">
         <div className="header-left-container">
           <img src={Logo} alt="logo" className="logo" />
           <span className="header-logo-line">
@@ -15,8 +15,8 @@ const Header =() => {
       </a>
       <div className="header_option_container">
         <ul className="header-option-list">
-          <li className="header-option"><a href="/">Home</a></li>
-          <li className="header-option"><a>Our Projects</a></li>
+          <li className="header-option"><a href="/community">Home</a></li>
+          <li className="header-option"><a href="/community/projects">Our Projects</a></li>
           <li className="header-option"><a>Events</a></li>
           <li className="header-option"><a>Join TDS</a></li>
           <li className="header-option"><a href="https://www.github.com/DesignSystemsOSS" target="_blank">GitHub</a></li>
