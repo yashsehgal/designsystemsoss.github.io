006cf7d8fd596b86f75b906a649249128e79a5c4
yashsehgal <yashsehgal.work@gmail.com>
2021-04-15 11:35:58 +0530

    App Component added, Routes added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Home from './pages/Home';
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Router>
+        <Route path="/" exact component={Home} />
+      </Router>
+      <Footer />
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,33 @@
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import Header from './components/Header';
+
+// Tests to check and validate Header Options in the Header Component
+test('renders learn react link', () => {
+  render(<Header />);
+  const linkElement = screen.getByText(/Home/i);
+  expect(linkElement).toBeInTheDocument();
+});
 
 test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  render(<Header />);
+  const linkElement = screen.getByText(/Our Projects/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('renders learn react link', () => {
+  render(<Header />);
+  const linkElement = screen.getByText(/Events/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('renders learn react link', () => {
+  render(<Header />);
+  const linkElement = screen.getByText(/Join The DesignSystems/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('renders learn react link', () => {
+  render(<Header />);
+  const linkElement = screen.getByText(/GitHub/i);
   expect(linkElement).toBeInTheDocument();
 });
