56489cfaf1ce20679f755b34f37f1a3cc76aa6b2
yashsehgal <yashsehgal.work@gmail.com>
2021-04-20 23:30:12 +0530

    App - Routes modified

--- src/App.js
+++ src/App.js
@@ -4,20 +4,21 @@ import Footer from './components/Footer';
 import Home from './pages/Home';
 import Projects from './pages/Projects';
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import Guidelines from './pages/Guidelines';
+import Guidelines from './pages/Guidelines';  
+import { HashRouter, Link } from "react-router-dom";
 
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <Router>
+
+        {/* <HashRouter basename="/community">
+          <Route exact path="/community" component={Home} />
+          <Route path="/community/projects" exact component={Projects} />
+          <Route path="/community/guidelines" exact component={Guidelines} />
+        </HashRouter> */}
         
-        <Route path="/community" exact component={Home} />
-        <Route path="/community/projects" exact component={Projects} />
-        <Route path="/community/guidelines" exact component={Guidelines} />
-        
-      </Router>
       {/* Under Development Status */}
       <p id="under-development-badge">
         Glad to have you here! Currently, This website is Under-Development Phase.
